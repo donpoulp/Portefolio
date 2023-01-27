9e267ba36c250b4366b525317de4669d60e97df0
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2023-01-27 20:45:33 +0100

    mail.php modif 16

--- web/mail.php
+++ web/mail.php
@@ -9,13 +9,15 @@
 <?php
     if (isset($_POST['message']) || isset($_POST['sujet']) || isset($_POST['mail']) || isset($_POST['nom'])) {
         
+
+        header("Location: https://raphael-petrozzi-portefolio.herokuapp.com/#ContactezMoi");
+        exit;
         $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $_POST['message'], '');
         if($retour){
             echo '<p>Votre message a bien été envoyé.</p>';
         }
 
-        header("Location: https://raphael-petrozzi-portefolio.herokuapp.com/#ContactezMoi");
-        exit;
+
     }
     ?>
 </body>
