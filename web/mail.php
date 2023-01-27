ae8d417361930dad05594e560356738c2093f2eb
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2023-01-27 20:38:56 +0100

    mail.php modif 14

--- web/mail.php
+++ web/mail.php
@@ -12,10 +12,10 @@
         $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $_POST['message'], '');
         if($retour){
             echo '<p>Votre message a bien été envoyé.</p>';
-            header("Location: index.php#ContactezMoi");
-            exit;
         }
 
+        header("Location: index.php#ContactezMoi");
+        exit;
     }
     ?>
 </body>
