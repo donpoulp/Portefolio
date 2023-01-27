e5e62c15a05c345ca7b9f666bde8fcf95eb6e5c9
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2023-01-27 19:08:45 +0100

    mail.php modif 5

--- web/mail.php
+++ web/mail.php
@@ -9,15 +9,8 @@
 <?php
     if (isset($_POST['message']) || isset($_POST['sujet']) || isset($_POST['mail']) || isset($_POST['nom'])) {
         echo $_POST['nom'] , $_POST['sujet'] , $_POST['mail'] , $_POST['message'];
-        $entete  = 'MIME-Version: 1.0' . "\r\n";
-        $entete .= 'Content-type: text/html; charset=utf-8' . "\r\n";
-        $entete .= 'Reply-to: ' . $_POST['mail'];
-
-        $message = '<h1>Message envoyé depuis la page Contact de monsite.fr</h1>
-        <p><b>Email : </b>' . $_POST['mail'] . '<br>
-        <b>Message : </b>' . htmlspecialchars($_POST['message']) . '</p>';
-
-        $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $message, $entete);
+        
+        $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $_POST['message'], '');
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
 
