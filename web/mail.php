8a09d6fa61d762f676127f6017fd43877e181c7f
donpoulp <raphael.petrozzi@le-campus-numerique.fr>
2023-01-27 19:01:10 +0100

    mail.php modif 3

--- web/mail.php
+++ web/mail.php
@@ -1,19 +1,10 @@
-<!DOCTYPE html>
-<html lang="fr">
-<head>
-    <meta charset="utf-8">
-    <title>Envoi d'un message par formulaire</title>
-</head>
 
-<body>
 <?php
     if (isset($_POST['message']) || isset($_POST['sujet']) || isset($_POST['mail']) || isset($_POST['nom'])) {
 
-        $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $_POST['message'], 'From:' . $_POST['mail']);
+        $retour = mail('raphael.petrozzi@le-campus-numerique.fr', $_POST['sujet'], $_POST['message'], '');
         if($retour)
             echo '<p>Votre message a bien été envoyé.</p>';
 
     }
     ?>
-</body>
-</html>
